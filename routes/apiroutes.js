bab2011c166e7b47330dcb890adb4d059c1123b6
Haider Shahid <haidershahid3.16@live.com>
2023-11-06 21:45:34 +1100

    missing starter files copied over

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -2,15 +2,19 @@ const noteData = require("../db/note_data");
 
 module.exports = function (app) {
   app.get("/api/notes/", function (req, res) {
+    console.log("get /api/notes");
+    
     res.json(noteData);
   });
 
   app.post("/api/notes/", function (req, res) {
+    console.log("post /api/notes");
     noteData.push(req.body);
     res.json(true);
   });
 
   app.delete("/api/notes/", function (req, res) {
+    console.log("delete /api/notes");
     noteData.length = 0;
     res.json({ ok: true });
   });
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,10 +2,14 @@ const path = require("path");
 
 module.exports = function (app) {
   app.get("/notes", function (req, res) {
+    console.log("get /notes");
+    console.log("path= ",path.join(__dirname, "../public/notes.html"));
     res.sendFile(path.join(__dirname, "../public/notes.html"));
   });
 
   app.get("*", function (req, res) {
+    console.log("get /*");
+    console.log("path= ",path.join(__dirname, "../public/index.html"));
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
 };
