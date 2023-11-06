ce931e1afe04ad067828fb8c2f980bcd69863053
Haider Shahid <haidershahid3.16@live.com>
2023-11-06 21:52:02 +1100

    port change and file name change

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 3001;
 
 // Middleware to parse JSON and URL-encoded data
 app.use(express.urlencoded({ extended: true }));
